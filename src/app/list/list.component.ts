7978c1d1dc761876c11980e737baf70ffc622a5f
angels101 <angelscodex101@gmail.com>
2020-08-27 17:12:48 +0300

    importing service on the list component i.e the HttpService

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  constructor(private _http: HttpService) { }
 
-  ngOnInit(): void {
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this._http.myMethod();gi
   }
 
 }
