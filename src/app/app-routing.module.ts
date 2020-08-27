872cb6ac394d83dc7c9526ff617f1356240a07e9
angels101 <angelscodex101@gmail.com>
2020-08-27 17:01:21 +0300

    importing ListComponent and adding to the const route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NavComponent} from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
-import {ListComponent} from './list/list.component'
+import {ListComponent} from './list/list.component';
 
 
 
