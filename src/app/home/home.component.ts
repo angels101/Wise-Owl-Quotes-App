732212ac5c15a5b064b12c0ace8769d25fc5d8db
angels101 <angelscodex101@gmail.com>
2020-08-27 17:07:12 +0300

    ng If and ngIfElse

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-clickCounter: number =  0;
+clickCounter =  0;
 name  = '';
 
   constructor() { }
@@ -15,9 +15,17 @@ name  = '';
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line: typedef
   countClick() {
 
 this.clickCounter += 1;
   }
+  setClasses(){
+    let myClasses = {
+   active: this.clickCounter > 4,
+   notactive: this.clickCounter <= 4,
+   }
+     return myClasses;
+   }
 
 }
