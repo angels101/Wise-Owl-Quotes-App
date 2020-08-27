6bcd04e143d7b07a42bf057c4dc13f7f061de535
angels101 <angelscodex101@gmail.com>
2020-08-27 13:00:13 +0300

    styling the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NavComponent} from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import {ListComponent} from './list/list.component'
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'nav', component: NavComponent},
+  { path: '', component: HomeComponent},
+  { path: 'list', component: ListComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { NavComponent } from './nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
